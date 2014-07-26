521f78fc51538a1a39bd6a54165f7abcd9e63b5c
skodape <p.skoda@centrum.cz>
2014-07-26 17:14:12 +0200

    New DPU t-zipper

---
+++ t-zipper/src/main/java/ue/unifiedviews/plugins/transformers/zipper/Configuration.java
@@ -0,0 +1,18 @@
+package ue.unifiedviews.plugins.transformers.zipper;
+
+public class Configuration {
+
+    private String zipFile = "data.zip";
+
+    public Configuration() {
+    }
+
+    public String getZipFile() {
+        return zipFile;
+    }
+
+    public void setZipFile(String zipFile) {
+        this.zipFile = zipFile;
+    }
+    
+}
