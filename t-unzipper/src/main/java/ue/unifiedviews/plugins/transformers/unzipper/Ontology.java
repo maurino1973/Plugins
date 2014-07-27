6af1e9070e46646640fd0ff489e081ca0387fd85
skodape <p.skoda@centrum.cz>
2014-07-27 09:29:06 +0200

    New DPU t-unzipper

---
+++ t-unzipper/src/main/java/ue/unifiedviews/plugins/transformers/unzipper/Ontology.java
@@ -0,0 +1,19 @@
+package ue.unifiedviews.plugins.transformers.unzipper;
+
+/**
+ *
+ * @author Škoda Petr
+ */
+public class Ontology {
+    
+    private Ontology() {
+        
+    }
+    
+    /**
+     * States that given file were packed into zip file.
+     */
+    public static String PREDICATE_EXTRACTED_FROM = 
+             "http://unifiedviews.cz/resource/domain/t-unzipper/source";
+    
+}
