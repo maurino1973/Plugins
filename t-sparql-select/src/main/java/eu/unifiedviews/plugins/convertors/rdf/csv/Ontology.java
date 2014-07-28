bd6c7a40c199a471d0ab6bcd148459efc4859628
skodape <p.skoda@centrum.cz>
2014-07-28 17:37:48 +0200

    New DPU t-sparqlTransformer

---
+++ t-sparql-select/src/main/java/eu/unifiedviews/plugins/convertors/rdf/csv/Ontology.java
@@ -0,0 +1,20 @@
+package eu.unifiedviews.plugins.convertors.rdf.csv;
+
+
+/**
+ *
+ * @author Škoda Petr
+ */
+public class Ontology {
+    
+    private Ontology() {
+        
+    }
+    
+    /**
+     * States that given file were packed into zip file.
+     */
+    public static String PREDICATE_SOURCE_GRAPH =
+             "http://unifiedviews.cz/resource/domain/t-sparql-select/source";
+    
+}
