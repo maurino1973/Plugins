5fdd2bbf375844bed15fc38c7148cd7e8b523e44
skodape <p.skoda@centrum.cz>
2014-07-28 16:56:09 +0200

    New DPU T-FilesFilter

---
+++ t-files-filter/src/main/java/ue/unifiedviews/plugins/transformers/files/filter/Configuration.java
@@ -0,0 +1,55 @@
+package ue.unifiedviews.plugins.transformers.files.filter;
+
+
+public class Configuration {
+
+    private String predicate = FixedPredicates.SYMBOLIC_NAME;
+
+    private String object = "*";
+
+    /**
+     * If false then {@link #predicate} represents one of {@link FixedPredicates}
+     * values and should be evaluated at runtime.
+     */
+    private boolean customPredicate = false;
+
+    private boolean useRegExp = true;
+
+    // TODO we can utilize also some option like: is part of string
+
+    public Configuration() {
+    }
+
+    public String getPredicate() {
+        return predicate;
+    }
+
+    public void setPredicate(String predicate) {
+        this.predicate = predicate;
+    }
+
+    public String getObject() {
+        return object;
+    }
+
+    public void setObject(String object) {
+        this.object = object;
+    }
+
+    public boolean isCustomPredicate() {
+        return customPredicate;
+    }
+
+    public void setCustomPredicate(boolean isCustom) {
+        this.customPredicate = isCustom;
+    }
+
+    public boolean isUseRegExp() {
+        return useRegExp;
+    }
+
+    public void setUseRegExp(boolean useRegExp) {
+        this.useRegExp = useRegExp;
+    }
+
+}
---
+++ t-files-filter/src/main/java/ue/unifiedviews/plugins/transformers/files/filter/FixedPredicates.java
@@ -0,0 +1,17 @@
+package ue.unifiedviews.plugins.transformers.files.filter;
+
+/**
+ *
+ * @author Škoda Petr
+ */
+public class FixedPredicates {
+
+    private FixedPredicates() {
+    
+    }
+
+    public static final String SYMBOLIC_NAME = "FIXED_SYMBOLIC_NAME"; 
+            
+    public static final String VIRTUAL_PATH = "VIRTUAL_PATH";
+
+}
