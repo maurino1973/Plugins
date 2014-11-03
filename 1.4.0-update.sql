36caa8ef116d0f6ba24e1b2f050b3ea517e4f477
Michal Klempa <mkl@eea.sk>
2014-11-03 06:07:55 +0100

    Bugfix in mysql script

--- 1.4.0-update.sql
+++ 1.4.0-update.sql
@@ -40,18 +40,18 @@ UPDATE `dpu_template` SET  `jar_name` = 'uv-t-tabular-1.4.0.jar'  WHERE `jar_nam
 UPDATE `dpu_template` SET  `jar_name` = 'uv-t-unzipper-1.4.0.jar'  WHERE `jar_name` LIKE 'uv-t-unzipper-%.jar';
 UPDATE `dpu_template` SET  `jar_name` = 'uv-t-xslt-1.4.0.jar'  WHERE `jar_name` LIKE 'uv-t-xslt-%.jar';
 UPDATE `dpu_template` SET  `jar_name` = 'uv-t-zipper-1.4.0.jar'  WHERE `jar_name` LIKE 'uv-t-zipper-%.jar';
-/*!40000 ALTER TABLE `dpu_template` ENABLE KEYS */  WHERE `jar_name` LIKE '%.jar';
-UNLOCK TABLES  WHERE `jar_name` LIKE '%.jar';
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */  WHERE `jar_name` LIKE '%.jar';
+/*!40000 ALTER TABLE `dpu_template` ENABLE KEYS */;
+UNLOCK TABLES; 
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */  WHERE `jar_name` LIKE '%.jar';
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */  WHERE `jar_name` LIKE '%.jar';
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */  WHERE `jar_name` LIKE '%.jar';
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */  WHERE `jar_name` LIKE '%.jar';
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */  WHERE `jar_name` LIKE '%.jar';
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */  WHERE `jar_name` LIKE '%.jar';
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */  WHERE `jar_name` LIKE '%.jar';
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2014-09-11 15:41:45
-DELETE FROM `properties` WHERE `key` = 'UV.Plugins.version'  WHERE `jar_name` LIKE '%.jar';
-INSERT INTO `properties` SET `key` = 'UV.Plugins.version', `value` = '001.004.000'  WHERE `jar_name` LIKE '%.jar';
+DELETE FROM `properties` WHERE `key` = 'UV.Plugins.version';
+INSERT INTO `properties` SET `key` = 'UV.Plugins.version', `value` = '001.004.000';
